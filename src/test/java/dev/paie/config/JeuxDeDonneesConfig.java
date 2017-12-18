7d6ca326009724a5cb2a909b6a3c7c7a78b9379e
MAWAAW <dark1111@hotmail.fr>
2017-12-18 17:10:40 +0100

    refactoring

--- src/test/java/dev/paie/config/JeuxDeDonneesConfig.java
+++ src/test/java/dev/paie/config/JeuxDeDonneesConfig.java
@@ -1,11 +1,13 @@
 package dev.paie.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 import dev.paie.entite.BulletinSalaire;
 
 @Configuration
+@ComponentScan({"dev.paie.util"})
 @ImportResource("classpath:jdd-config.xml")
 public class JeuxDeDonneesConfig {
 
